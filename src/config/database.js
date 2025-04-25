4bf34478ceb632bbcf35a84b056db11f2cf3d4c6
ClaudiusRkt <rakotonirinaclaudius@gmail.com>
2025-04-25 16:40:11 +0300

    MAJ2

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,21 @@
-const { Sequelize } = require('sequelize');
 require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
     logging: false
-});
+  }
+);
 
+// Message adapté pour MySQL
 sequelize.authenticate()
-    .then(() => console.log(" Connexion à MySQL réussie"))
-    .catch(err => console.error(" Erreur de connexion :", err));
+  .then(() => console.log("✅ Connexion à MySQL réussie"))
+  .catch(err => console.error("❌ Erreur de connexion à la base :", err));
 
 module.exports = sequelize;
-
